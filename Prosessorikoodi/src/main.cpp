7051aca1f7830226502da5a4a7cccce6b955d3a5
Matias Karjalainen <t1kama00@students.oamk.fi>
2024-04-19 12:04:01 +0300

    SD-kortin funktiot lisätty main.cpp

--- Prosessorikoodi/src/main.cpp
+++ Prosessorikoodi/src/main.cpp
@@ -55,11 +55,24 @@ ICM_20948_SPI myICM; // If using SPI create an ICM_20948_SPI object
 #else
 ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
 #endif
+
+//funktioita jotka ei ole omina kirjastoinaan salaisista syistä
 void printScaledAGMT(ICM_20948_SPI *sensor);
 void printSensorDataFloat(ICM_20948_SPI *sensor);
 void printRawAGMT(ICM_20948_AGMT_t agmt);
 void getSensorData(ICM_20948_SPI *sensor);
 void printSensorData();
+//SD:n käyttöfunktioiden julistukset
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels);
+void createDir(fs::FS &fs, const char * path);
+void removeDir(fs::FS &fs, const char * path);
+void readFile(fs::FS &fs, const char * path);
+void writeFile(fs::FS &fs, const char * path, const char * message);
+void appendFile(fs::FS &fs, const char * path, const char * message);
+void renameFile(fs::FS &fs, const char * path1, const char * path2);
+void deleteFile(fs::FS &fs, const char * path);
+void testFileIO(fs::FS &fs, const char * path);
+
 
 SPIClass spi = SPIClass(HSPI); //SD Card SPI
 
@@ -154,6 +167,21 @@ void initSDCard(){
   }
   uint64_t cardSize = SD.cardSize() / (1024 * 1024);
   Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+  listDir(SD, "/", 0);
+  createDir(SD, "/mydir");
+  listDir(SD, "/", 0);
+  removeDir(SD, "/mydir");
+  listDir(SD, "/", 2);
+  writeFile(SD, "/hello.txt", "Hello ");
+  appendFile(SD, "/hello.txt", "World!\n");
+  readFile(SD, "/hello.txt");
+  deleteFile(SD, "/foo.txt");
+  renameFile(SD, "/hello.txt", "/foo.txt");
+  readFile(SD, "/foo.txt");
+  testFileIO(SD, "/test.txt");
+  Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+  Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
 }
 
 void initSPIFFS() {
@@ -266,112 +294,6 @@ void setupWiFi(){
   server.begin();
   }
 
-}
-
-void setup()
-{
-  pinMode(LED1,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  pinMode(BTN1, INPUT_PULLUP);
-  pinMode(BTN2, INPUT_PULLUP); 
-  SERIAL_PORT.begin(115200);
-  
-  //initSPIFFS();  //ESP32 internal file system
-  initMPU();
-  initSDCard();
-
-  
-}
-
-void loop()
-{
-  // read the state of the switch/button:
-  btn1State = digitalRead(BTN1);
-  btn2State = digitalRead(BTN2);
-
-  if(btn1StateOld == HIGH && btn1State == LOW)
-  {
-    Serial.println("Button 1 pressed");
-    digitalWrite(LED2, HIGH);
-    IMU = true;
-    delay(100);
-  }
-  btn1StateOld = btn1State;
-   if(btn2StateOld == HIGH && btn2State == LOW)
-  {
-    Serial.println("Button 2 pressed");
-    digitalWrite(LED2, LOW);
-    IMU = false;
-    delay(100);
-  }
-  btn2StateOld = btn2State;
-
-  if(btn1State == LOW && btn2State == LOW)
-  {
-    Serial.println("Both buttons pressed");
-    wifi = !wifi;
-    Serial.println("Wifi boolean: " + String(wifi));
-    if(wifi)
-    {
-      setupWiFi();
-    }
-    else
-    {
-      server.end();
-      Serial.println("Server stopped");
-      WiFi.disconnect();
-      Serial.println("Wifi disconnected");
-    }
-    delay(100);
-  }
-
-  // Read the sensor data
-  if(IMU)
-  {
-     if (myICM.dataReady())
-  {
-    myICM.getAGMT();
-    //printRawAGMT( myICM.agmt );
-    getSensorData(&myICM);
-    printSensorData();
-    delay(100);
-  }
-  else
-  {
-    SERIAL_PORT.println("Waiting for data");
-    delay(500);
-  }
-  }
-  
-
-  //Send data to the web server
-
-  if(wifi)
-  {
-    if ((millis() - lastTime) > gyroDelay) {
-    // Send Events to the Web Server with the Sensor Readings
-    events.send(getGyroReadings().c_str(),"gyro_readings",millis());
-    lastTime = millis();
-    }
-    if ((millis() - lastTimeAcc) > accelerometerDelay) {
-    // Send Events to the Web Server with the Sensor Readings
-    events.send(getAccReadings().c_str(),"accelerometer_readings",millis());
-    lastTimeAcc = millis();
-    }
-    if ((millis() - lastTimeMag) > magDelay) {
-    // Send Events to the Web Server with the Sensor Readings
-    events.send(getMagReadings().c_str(),"magnetometer_readings",millis());
-    lastTimeMag = millis();
-    }
-    if ((millis() - lastTimeTemperature) > temperatureDelay) {
-    // Send Events to the Web Server with the Sensor Readings
-    events.send(getTemperature().c_str(),"temperature_reading",millis());
-    lastTimeTemperature = millis();
-    }
-  }
-
-  //Turn led1 on that indicates we are end of the loop function
-  digitalWrite(LED1, HIGH);
 }
 
 void getSensorData(ICM_20948_SPI *sensor)
@@ -619,4 +541,266 @@ void printSensorDataFloat(ICM_20948_SPI *sensor)
   SERIAL_PORT.print(magZ);
   SERIAL_PORT.println(" uT");
   SERIAL_PORT.println();
+}
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+  Serial.printf("Listing directory: %s\n", dirname);
+
+  File root = fs.open(dirname);
+  if(!root){
+    Serial.println("Failed to open directory");
+    return;
+  }
+  if(!root.isDirectory()){
+    Serial.println("Not a directory");
+    return;
+  }
+
+  File file = root.openNextFile();
+  while(file){
+    if(file.isDirectory()){
+      Serial.print("  DIR : ");
+      Serial.println(file.name());
+      if(levels){
+        listDir(fs, file.name(), levels -1);
+      }
+    } else {
+      Serial.print("  FILE: ");
+      Serial.print(file.name());
+      Serial.print("  SIZE: ");
+      Serial.println(file.size());
+    }
+    file = root.openNextFile();
+  }
+}
+
+void createDir(fs::FS &fs, const char * path){
+  Serial.printf("Creating Dir: %s\n", path);
+  if(fs.mkdir(path)){
+    Serial.println("Dir created");
+  } else {
+    Serial.println("mkdir failed");
+  }
+}
+
+void removeDir(fs::FS &fs, const char * path){
+  Serial.printf("Removing Dir: %s\n", path);
+  if(fs.rmdir(path)){
+    Serial.println("Dir removed");
+  } else {
+    Serial.println("rmdir failed");
+  }
+}
+
+void readFile(fs::FS &fs, const char * path){
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+  if(!file){
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.print("Read from file: ");
+  while(file.available()){
+    Serial.write(file.read());
+  }
+  file.close();
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file){
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)){
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file){
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)){
+      Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+  Serial.printf("Renaming file %s to %s\n", path1, path2);
+  if (fs.rename(path1, path2)) {
+    Serial.println("File renamed");
+  } else {
+    Serial.println("Rename failed");
+  }
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+  Serial.printf("Deleting file: %s\n", path);
+  if(fs.remove(path)){
+    Serial.println("File deleted");
+  } else {
+    Serial.println("Delete failed");
+  }
+}
+
+void testFileIO(fs::FS &fs, const char * path){
+  File file = fs.open(path);
+  static uint8_t buf[512];
+  size_t len = 0;
+  uint32_t start = millis();
+  uint32_t end = start;
+  if(file){
+    len = file.size();
+    size_t flen = len;
+    start = millis();
+    while(len){
+      size_t toRead = len;
+      if(toRead > 512){
+        toRead = 512;
+      }
+      file.read(buf, toRead);
+      len -= toRead;
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes read for %u ms\n", flen, end);
+    file.close();
+  } else {
+    Serial.println("Failed to open file for reading");
+  }
+
+
+  file = fs.open(path, FILE_WRITE);
+  if(!file){
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+
+  size_t i;
+  start = millis();
+  for(i=0; i<2048; i++){
+    file.write(buf, 512);
+  }
+  end = millis() - start;
+  Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+  file.close();
+}
+
+void setup()
+{
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+  pinMode(BTN1, INPUT_PULLUP);
+  pinMode(BTN2, INPUT_PULLUP); 
+  SERIAL_PORT.begin(115200);
+  
+  //initSPIFFS();  //ESP32 internal file system
+  initMPU();
+  initSDCard();
+
+}
+
+void loop()
+{
+  // read the state of the switch/button:
+  btn1State = digitalRead(BTN1);
+  btn2State = digitalRead(BTN2);
+
+  if(btn1StateOld == HIGH && btn1State == LOW)
+  {
+    Serial.println("Button 1 pressed");
+    digitalWrite(LED2, HIGH);
+    IMU = true;
+    delay(100);
+  }
+  btn1StateOld = btn1State;
+   if(btn2StateOld == HIGH && btn2State == LOW)
+  {
+    Serial.println("Button 2 pressed");
+    digitalWrite(LED2, LOW);
+    IMU = false;
+    delay(100);
+  }
+  btn2StateOld = btn2State;
+
+  if(btn1State == LOW && btn2State == LOW)
+  {
+    Serial.println("Both buttons pressed");
+    wifi = !wifi;
+    Serial.println("Wifi boolean: " + String(wifi));
+    if(wifi)
+    {
+      setupWiFi();
+    }
+    else
+    {
+      server.end();
+      Serial.println("Server stopped");
+      WiFi.disconnect();
+      Serial.println("Wifi disconnected");
+    }
+    delay(100);
+  }
+
+  // Read the sensor data
+  if(IMU)
+  {
+     if (myICM.dataReady())
+  {
+    myICM.getAGMT();
+    //printRawAGMT( myICM.agmt );
+    getSensorData(&myICM);
+    printSensorData();
+    delay(100);
+  }
+  else
+  {
+    SERIAL_PORT.println("Waiting for data");
+    delay(500);
+  }
+  }
+  
+
+  //Send data to the web server
+
+  if(wifi)
+  {
+    if ((millis() - lastTime) > gyroDelay) {
+    // Send Events to the Web Server with the Sensor Readings
+    events.send(getGyroReadings().c_str(),"gyro_readings",millis());
+    lastTime = millis();
+    }
+    if ((millis() - lastTimeAcc) > accelerometerDelay) {
+    // Send Events to the Web Server with the Sensor Readings
+    events.send(getAccReadings().c_str(),"accelerometer_readings",millis());
+    lastTimeAcc = millis();
+    }
+    if ((millis() - lastTimeMag) > magDelay) {
+    // Send Events to the Web Server with the Sensor Readings
+    events.send(getMagReadings().c_str(),"magnetometer_readings",millis());
+    lastTimeMag = millis();
+    }
+    if ((millis() - lastTimeTemperature) > temperatureDelay) {
+    // Send Events to the Web Server with the Sensor Readings
+    events.send(getTemperature().c_str(),"temperature_reading",millis());
+    lastTimeTemperature = millis();
+    }
+  }
+
+  //Turn led1 on that indicates we are end of the loop function
+  digitalWrite(LED1, HIGH);
 }
